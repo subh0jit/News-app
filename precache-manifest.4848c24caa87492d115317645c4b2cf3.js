1ed2cfc04f63ed8de17e53a10fdc0be8c9ebb59e
subh0jit <subhojitz0123@gmail.com>
2020-04-17 21:59:00 +0530

    Updates

---
+++ precache-manifest.4848c24caa87492d115317645c4b2cf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c93821cc6bf241b14534561cab652d",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "d12b3c52562cf3a50107",
+    "url": "/news-app/static/css/main.5db1e149.chunk.css"
+  },
+  {
+    "revision": "a88bee6cc9ceb1a7dee6",
+    "url": "/news-app/static/js/2.6523c205.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/news-app/static/js/2.6523c205.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d12b3c52562cf3a50107",
+    "url": "/news-app/static/js/main.0fcde199.chunk.js"
+  },
+  {
+    "revision": "906f5a3a64ce57a52fc9",
+    "url": "/news-app/static/js/runtime-main.7b1f5c87.js"
+  },
+  {
+    "revision": "6c645e7d6db1efcbb6f3bdfe24aeadf6",
+    "url": "/news-app/static/media/logo.6c645e7d.svg"
+  }
+]);
